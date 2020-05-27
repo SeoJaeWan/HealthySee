40da43336e9e8fec8b82f50faee5b75772b05121
hanapple07 <54665408+hanapple07@users.noreply.github.com>
2020-05-27 09:31:59 +0900

    Add files via upload

--- 로그인/로그인_Front WEB/src/component_frame/component/Header.js
+++ 로그인/로그인_Front WEB/src/component_frame/component/Header.js
@@ -1,43 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import {
-  Container,
-  Element,
-  Shortcut,
-  Menu,
-  Title,
-  Login,
-} from "../style/Header_Style.js";
+import { Container, Menu, Title, Login } from "../style/Header_Style.js";
 
 const Header = ({ user }) => {
   return (
     <Container>
-      <Element>
-        <Shortcut>
-          <Menu>메뉴</Menu>
-          <Title>
-            <Link to="/">
-              <img src={require("../../Images/Header/Title.png")} alt="" />
-            </Link>
-          </Title>
-          <Login>
-            {user ? (
-              <>
-                <Link>{user.username}</Link>
+      <Menu>메뉴</Menu>
+      <Link to="/">
+        <Title src={require("../../Images/Header/Title.png")} alt="" />
+      </Link>
+      <Login>
+        {user ? (
+          <>
+            <Link>{user.username}</Link>
 
-                <Link>로그아웃</Link>
-              </>
-            ) : (
-              <>
-                <Link to="/Login">로그인</Link>
-
-                <Link to="/Signup">회원가입</Link>
-              </>
-            )}
-          </Login>
-        </Shortcut>
-      </Element>
+            <Link>로그아웃</Link>
+          </>
+        ) : (
+          <>
+            <Link to="/Login">로그인</Link>/<Link to="/Signup">회원가입</Link>
+          </>
+        )}
+      </Login>
     </Container>
   );
 };
--- 로그인/로그인_Front WEB/src/component_frame/style/Header_Style.js
+++ 로그인/로그인_Front WEB/src/component_frame/style/Header_Style.js
@@ -1,25 +1,12 @@
 import styled from "styled-components";
+import { media } from "../../ReactiveStyle/ReactiveStyle";
 
 export const Container = styled.div`
   width: 100%;
+  height: 70px;
   border-bottom: 10px solid #676a72;
 `;
 
-export const Element = styled.div`
-  margin: 0 auto;
-  width: 100%;
-  height: 80px;
-  font-size: 15pt;
-  display: flex;
-  flex-flow: row wrap;
-`;
-
-export const Shortcut = styled.div`
-  order: 1;
-  width: 100%;
-  height: 30px;
-  margin-top: 10px;
-`;
 export const Menu = styled.button`
   position: absolute;
   font-size: 20pt;
@@ -30,15 +17,31 @@ export const Menu = styled.button`
   color: white;
   border-radius: 10%;
   left: 1%;
+  ${media.mobile`
+`}
 `;
 
-export const Title = styled.div`
-  position: absolute;
-  right: 40%;
+export const Title = styled.img`
+  position:relative;
+  left:30%;
+  width: auto; height: auto;
+  max-width: 500px;
+  max-height: 500px;
+  ${media.mobile`
+  width: 50%;
+  top:10%;
+  left:25%;
+`}
 `;
 
 export const Login = styled.div`
   position: absolute;
+  font-size: 20pt;
   top: 3.5%;
   right: 3%;
+  ${media.mobile`
+  margin-top: 0.5rem;
+  font-size:5pt;
+
+`}
 `;
