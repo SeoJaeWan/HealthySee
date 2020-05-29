707bcedaf53452a8917897311dc178e3734497e3
JunGeunHong1129 <54523136+JunGeunHong1129@users.noreply.github.com>
2020-05-29 22:15:46 +0900

    Update jwtMiddlewares.js
    0529 홍준근

--- 로그인/로그인_backend/token/jwtMiddlewares.js
+++ 로그인/로그인_backend/token/jwtMiddlewares.js
@@ -23,7 +23,7 @@ const getRefreshToken = () => {
 const jwtMiddleware = async (req, res, next) => {
   const token = req.cookies.access_token;
   console.log(req.cookies.access_token);
-  console.log('asdf',token);
+  console.log('asdf', token);
   if (!token) {
     return next();
   }
@@ -49,37 +49,47 @@ const jwtMiddleware = async (req, res, next) => {
     return next();
   } catch (error) {
     console.log(error);
-    const refreshToken = await Token.findOne({
-      where: {
-        TK_Refresh_Token: req.cookies.refresh,
-      },
-    });
-      if(refreshToken){
+    const rftoken = req.cookies.refresh;
+    if (rftoken) {
+      const refreshToken = await Token.findOne({
+        where: {
+          TK_Refresh_Token: rftoken,
+        },
+      });
+      if (refreshToken) {
         console.log('엑세스 토큰 재생성 시작');
-        const rfDecode=jwt.verify(refreshToken.TK_Refresh_Token, process.env.JWT_SECRET);
-          if(rfDecode){
+        var date = (Date.now() - Date(refreshToken.TK_Creation_Date)) / 24 * 60 * 60 * 1000;
+        if (parseInt(date) > 0) {
+
             req.body.user = {
-              username : refreshToken.TK_NickName,
-              email : refreshToken.TK_Platform_Account,
+              username: refreshToken.TK_NickName,
+              email: refreshToken.TK_Platform_Account,
             };
+
             const token = getToken(req.body.user);
 
             res.cookie("access_token", token, {
               maxAge: 1000 * 60 * 30, // 30분
               httpOnly: true,
-            });
-            return next();
-          }else{
-            // 리프레시 토큰 기간 만료, 다시 로그인
-            console.log('This refresh token is not vaild')
-            res.status(401).send("Login Again");
-            return next();
-          }
-        
-      }else{
-          // 리프레시 토큰 존재x , 다시 로그인
+            });        
+
+        }else{
+          // 리프레시 토큰기간 만료, db에서 삭제
+          User.delete({
+            where: {
+              TK_Refresh_Token: rftoken,
+            }
+          });
+
+          return next();
+        }
+      } else {
+        // 리프레시 토큰 존재x , 다시 로그인
         return next();
       }
+    }
+    //쿠키에 존재x
+    return next();
   }
 };
 
