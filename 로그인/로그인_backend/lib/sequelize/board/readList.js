500035864a1885251ae0849342e971560151626c
SeoJaeWan <52366178+SeoJaeWan@users.noreply.github.com>
2020-06-13 00:30:14 +0900

    Add files via upload

--- 로그인/로그인_backend/lib/sequelize/board/readList.js
+++ 로그인/로그인_backend/lib/sequelize/board/readList.js
@@ -5,28 +5,27 @@ const readlList = async (req, res, next) => {
   keyword = req.query.keyword;
   BO_Code = req.query.BO_Code;
   category = req.query.category;
-  console.log(BO_Code);
+  console.log("asdasdasdasdsadsadsdasda", req.query);
   var responseData = {};
   var condition = {
     where: {
       [Op.and]: [
         name
-          ? (name === 'BO_Title')
-              ?{BO_Title: { [Op.like]: "%" + keyword + "%" },}
-              :{ BO_Writer_NickName: { [Op.like]: "%" + keyword + "%" },}
+          ? name === "BO_Title"
+            ? { BO_Title: { [Op.like]: "%" + keyword + "%" } }
+            : { BO_Writer_NickName: { [Op.like]: "%" + keyword + "%" } }
           : null,
-        BO_Code
-         ?{ BO_Code: { [Op.lt]: BO_Code } }
-         : null,
+        BO_Code ? { BO_Code: { [Op.lt]: BO_Code } } : null,
         { BO_Category: category },
       ],
     },
     limit: 10,
   };
-  var boardList = await BoardList.findAll(condition);
-  responseData.boardList = boardList;
+  var boardList = await BoardList.findAndCountAll(condition);
+  console.log(boardList.rows);
+  responseData.boardList = boardList.rows;
+  responseData.boardCount = boardList.count;
   return res.json(responseData);
 };
-
 
 module.exports = readlList;
