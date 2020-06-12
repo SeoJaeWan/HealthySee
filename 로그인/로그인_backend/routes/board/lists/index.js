e4e73e1e0ce7c68f640ba9074773c120b6d2ce69
jpoh281 <54665433+jpoh281@users.noreply.github.com>
2020-06-12 22:05:25 +0900

    Add files via upload

--- 로그인/로그인_backend/routes/board/lists/index.js
+++ 로그인/로그인_backend/routes/board/lists/index.js
@@ -1,16 +1,13 @@
 var express = require("express");
 var router = express.Router();
-const readGeneralList = require("../../../lib/sequelize/board/readList").readGeneralList;
-const readHealthList = require("../../../lib/sequelize/board/readList").readHealthList;
+const readList = require("../../../lib/sequelize/board/readList");
 const { Op } = require("sequelize");
 
 // 1. /search 게시글 전체 검색
 // 반환 값 -> 게시글 20개 { BO_CODE, BO_TITLE, BO_HIT, 해당 BO_CODE와 관련된 댓글 수, 추천 수 등등 }
 
-router.get("/1/", readHealthList);
-router.get("/0/", readGeneralList);
 
-router.get("/0/:BO_Code", readGeneralList);
-router.get("/1/:BO_Code", readHealthList);
+router.get("/", readList);
+
 
 module.exports = router;
