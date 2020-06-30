bf1fb25e3b10abd40216bfda5199e33eed77abe3
hanapple07 <54665408+hanapple07@users.noreply.github.com>
2020-07-01 03:38:40 +0900

    Add files via upload

--- 로그인/로그인_Front WEB/src/Page/MyPage.js
+++ 로그인/로그인_Front WEB/src/Page/MyPage.js
@@ -4,6 +4,7 @@ import List from "../component_frame/component/List";
 import MyPageCome from "../component_contet/component/MyPage/MyPageCom";
 import { Route } from "react-router-dom";
 import MyPageEditCome from "../component_contet/component/MyPage/MyPageEditCom";
+import RegisterBO from "../component_contet/component/MyPage/Register/RegisterBO";
 import HeaderForm from "../containers/Header/HeaderForm";
 
 class MyPage extends Component {
@@ -14,6 +15,7 @@ class MyPage extends Component {
         <List />
         <Route exact path="/MyPage" component={MyPageCome} />
         <Route path="/MyPage/edit" component={MyPageEditCome} />
+        <Route path="/MyPage/registerEX" component={RegisterBO} />
       </>
     );
   }
