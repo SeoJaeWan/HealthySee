909d084c83a9e3200f36db4e0639f01b856528b7
hanapple07 <54665408+hanapple07@users.noreply.github.com>
2020-07-03 08:44:04 +0900

    Add files via upload

--- 로그인/로그인_Front WEB/src/component_contet/common/Pagenation.js
+++ 로그인/로그인_Front WEB/src/component_contet/common/Pagenation.js
@@ -1,17 +1,25 @@
 import React from "react";
 import styled from "styled-components";
 import { Container } from "../style/Container.js";
+import { Pagenationform } from "./style/Pagenation_Style.js";
 
 const Pagenation = ({ page, getPage }) => {
   return (
     <Container>
-      {[...Array(page)].map((count, index) => {
-        return (
-          <Button key={index} onClick={() => getPage(index + 1)}>
-            {index + 1}
-          </Button>
-        );
-      })}
+      <Pagenationform>
+        <button className="Ltbutton">&lt;</button>
+        <div className="pageform">
+
+          {[...Array(page)].map((count, index) => {
+            return (
+              <Button key={index} onClick={() => getPage(index + 1)}>
+                {index + 1}
+              </Button>
+            );
+          })}
+        </div>
+        <button className="Rtbutton">&gt;</button>
+      </Pagenationform>
     </Container>
   );
 };
