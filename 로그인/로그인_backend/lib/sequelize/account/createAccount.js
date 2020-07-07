98fcb3d10aab93cf1c46e313489b33c2b61d3cd4
JunGeunHong1129 <54523136+JunGeunHong1129@users.noreply.github.com>
2020-07-07 16:14:19 +0900

    07/07 홍준근
    멤버디비에 맴버 생성 추가

--- 로그인/로그인_backend/lib/sequelize/account/createAccount.js
+++ 로그인/로그인_backend/lib/sequelize/account/createAccount.js
@@ -1,5 +1,6 @@
 var today = require("../../../lib/Date/time");
 const Account = require("../../../models").account;
+const Member = require("../../../models").member;
 const platforms = {
   kakao: 1,
   naver: 2,
@@ -21,14 +22,19 @@ const createAccount = async (req, res, next) => {
   } else {
     await Account.create({
       AC_NickName: nickname,
-      AC_Gender: gender,
-      AC_Weight: weight,
-      AC_Scope: scope,
       AC_Name: name,
       AC_Platform: platforms[platform],
       AC_Platform_Account: email,
       AC_Creation_Date: today,
       AC_Last_Access_Date: today,
+    });
+
+    await Member.create({
+      ME_Scope: scope,
+      ME_Weight: weight,
+      ME_Gender: gender,
+      ME_Certificate: "user",
+      Account_AC_NickName: nickname
     });
   }
 
