f55e389ee2d0f5e904ac763aa3e69216e9a66669
hanapple07 <54665408+hanapple07@users.noreply.github.com>
2020-07-09 09:13:43 +0900

    Update index.js

--- 로그인/로그인_Front WEB/src/Page/Board/index.js
+++ 로그인/로그인_Front WEB/src/Page/Board/index.js
@@ -1,21 +1,24 @@
-import React from "react";
+import React from "react"
 
-import { Route } from "react-router-dom";
-import ReadPost from "./ReadPost";
-import WritePost from "./WritePost";
-import BoardPost from "./BoardPost";
-import SelectBoard from "./SelectBoard";
+import { Route, Switch } from "react-router-dom"
+
+import ReadPost from "./ReadPost"
+import WritePost from "./WritePost"
+import BoardPost from "./BoardPost"
+import SelectBoard from "./SelectBoard"
 
 const Board = ({ match }) => {
   return (
     <>
-      <Route exact path={match.path} component={BoardPost} />
-      <Route path="/SelectBoard" component={SelectBoard} />
+      <Switch>
+        <Route exact path="/Board/Select" component={SelectBoard} />
+        <Route exact path={match.path} component={BoardPost} />
+      </Switch>
       <Route exact path={`${match.path}/search`} component={BoardPost} />
       <Route path={`${match.path}/write`} component={WritePost} />
       <Route path={`${match.path}/read/:postId`} component={ReadPost} />
     </>
-  );
-};
+  )
+}
 
-export default Board;
+export default Board
