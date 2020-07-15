758e760458d1859240523e27af55cf3e225b39f0
SeoJaeWan <sjw7324@gmail.com>
2020-07-15 10:46:41 +0900

    마이페이지 수정

--- 프로젝트/Web_Front/src/Page/MyPage.js
+++ 프로젝트/Web_Front/src/Page/MyPage.js
@@ -1,15 +1,13 @@
-import React, { Component } from "react"
+import React, { Component } from "react";
 
-import { Route } from "react-router-dom"
-import RegisterBO from "../component_contet/component/MyPage/Register/RegisterBOCom"
-import RegisterWrite from "../component_contet/component/MyPage/Register/RegisterWriteCom"
-import RegisterRead from "../component_contet/component/MyPage/Register/RegisterReadCom"
-import RegisterTR from "../component_contet/component/MyPage/RegisterTRCom"
-import Album from "../component_contet/component/MyPage/Album/Album"
-import AlbumWrite from "../component_contet/component/MyPage/Album/AlbumWrite"
+import { Route } from "react-router-dom";
+import RegisterBO from "../component_contet/component/MyPage/Register/RegisterBOCom";
+import RegisterWrite from "../component_contet/component/MyPage/Register/RegisterWriteCom";
+import RegisterRead from "../component_contet/component/MyPage/Register/RegisterReadCom";
+import RegisterTR from "../component_contet/component/MyPage/RegisterTRCom";
 
-import MypageForm from "../containers/mypage/MypageForm"
-import MypageEditForm from "../containers/mypage/MypageEditForm"
+import MypageForm from "../containers/mypage/MypageForm";
+import MypageEditForm from "../containers/mypage/MypageEditForm";
 
 class MyPage extends Component {
   render() {
@@ -17,14 +15,12 @@ class MyPage extends Component {
       <>
         <Route path="/MyPage/Edit" component={MypageEditForm} />
         <Route path="/MyPage/registerTR" component={RegisterTR} />
-        <Route exact path="/MyPage/Album" component={Album} />
-        <Route path="/MyPage/Album/write" component={AlbumWrite} />
         <Route exact path="/MyPage/registerBO" component={RegisterBO} />
         <Route path="/MyPage/registerBO/write" component={RegisterWrite} />
         <Route path="/MyPage/registerBO/read" component={RegisterRead} />
         <Route exact path="/MyPage/:username/Home" component={MypageForm} />
       </>
-    )
+    );
   }
 }
-export default MyPage
+export default MyPage;
