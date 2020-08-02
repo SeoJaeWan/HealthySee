700765365bfdd9944581c3ae16a9dfa00d8ad0f5
jpoh281 <54665433+jpoh281@users.noreply.github.com>
2020-08-02 12:28:32 +0900

    카테고리 삭제

--- 프로젝트/BackEnd/lib/sequelize/board/readList.js
+++ 프로젝트/BackEnd/lib/sequelize/board/readList.js
@@ -5,7 +5,6 @@ const readList = async (req, res, next) => {
   name = req.query.name;
   keyword = req.query.keyword;
   BO_Code = req.query.BO_Code;
-  category = req.query.category;
   console.log(BO_Code);
   var responseData = {};
   var condition = {
@@ -13,7 +12,6 @@ const readList = async (req, res, next) => {
       [Op.and]: [
         name && { [name]: { [Op.like]: "%" + keyword + "%" } },
         BO_Code && { BO_Code: { [Op.lt]: BO_Code } },
-        { BO_Category: category },
       ],
     },
     limit: 10,
@@ -28,51 +26,19 @@ const readList = async (req, res, next) => {
 const bestList = async (req, res, next) => {
   let name = req.params.name;
   let count = Number(req.params.count);
-
   let responseData = {};
-  //   condition = {
-  //     where: {
-  //       [Op.and]: [
-  //         { BO_Creation_Date : {[Op.gt]: new Date(new Date() - 72 * 60 * 60 * 1000)}},
-  //         { BO_Category: category },
-  //       ],
-  //     },
-  //   order : [[name],"DESC"],
-  //   limit: 3}
-  // }
-
   bestListFree = await BoardList.findAll({
-    where: {
-      [Op.and]: [
+    where: 
         {
           BO_Creation_Date: {
             [Op.gt]: new Date(new Date() - 96 * 60 * 60 * 1000),
           },
         },
-        { BO_Category: 0 },
-      ],
-    },
-    order: [[`${name}`, "DESC"]],
-    limit: count,
-  });
-
-  bestListExer = await BoardList.findAll({
-    where: {
-      [Op.and]: [
-        {
-          BO_Creation_Date: {
-            [Op.gt]: new Date(new Date() - 96 * 60 * 60 * 1000),
-          },
-        },
-        { BO_Category: 1 },
-      ],
-    },
     order: [[`${name}`, "DESC"]],
     limit: count,
   });
 
   responseData.free = bestListFree;
-  responseData.exer = bestListExer;
   res.json(responseData);
 };
 
