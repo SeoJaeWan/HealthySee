6dddc800d8a4ce403f6b5c83e1ed200395cfacaa
JunGeunHong1129 <54523136+JunGeunHong1129@users.noreply.github.com>
2020-08-04 16:33:14 +0900

    08/04 홍준근
    readAlbum 커맨트 추가

--- 프로젝트/BackEnd/lib/sequelize/album/readAlbum.js
+++ 프로젝트/BackEnd/lib/sequelize/album/readAlbum.js
@@ -1,8 +1,7 @@
 const A_Comment = require("../../../models").a_comment;
 const album = require("../../../models").album;
 const { Op } = require("sequelize");
-const FrontComment = require("../../../models").frontcomment;
-const Picture = require("../../../models").picture;
+const Picture = require("../../../models").a_picture;
 
 const readPost = async (req, res, next) => {
   var AL_Code = req.params.AL_Code;
@@ -12,27 +11,22 @@ const readPost = async (req, res, next) => {
   var Album = await album.findOne({
     where: { AL_Code },
   });
+  // 블럭된 유저인지 체크
+
 
   var pictures = await Picture.findAll({
       where : {Album_AL_Code : Album.AL_Code},
   });
 
-  // if(pictures){
-  //     for(i = 0; i < pictures.length ; i++){
-  //       Album.AL_Picture[i] = pictures[i].P_Picture;
-  //     }
-  // }
-  // console.log(Album);
 
   // 댓글 1페이지
-//   comments = await FrontComment.findAndCountAll({
-//     where: { Board_BO_Code: AL_Code },
-//     order: [
-//       ["BC_Re_Ref", "DESC"],
-//       ["BC_Code", "ASC"],
-//     ],
-//     limit : 20,
-//   });
+  comments = await A_Comment.findAndCountAll({
+    where: { Album_AL_Code: AL_Code },
+    order: [
+      ["ACO_Code", "ASC"],
+    ],
+    limit : 20,
+  });
 
   
   if(req.method === 'GET')
@@ -40,32 +34,20 @@ const readPost = async (req, res, next) => {
   else
     username = req.body.username;
 
-//  var isHealthsee = await B_Healthsee.count({
-//     where : {[Op.and]:[ 
-//       {Board_BO_Code: BO_Code },
-//       {BH_Push_NickName : username },
-// ]}})
-// var isReport = await B_Reporter.count({
-//   where : {[Op.and]:[ 
-//     {Board_BO_Code: BO_Code },
-//     {BR_Reporter_NickName : username },
-// ]}})
  
    responseData.albumDetail = Album;
-   responseData.picture = pictures;
-//  responseData.comments = comments.rows;
-//  responseData.lastPage = (Math.ceil(comments.count / 20) == 0)?1:Math.ceil(comments.count / 20);
-//   responseData.isHealthsee = isHealthsee;
-//   responseData.isReport = isReport;
-
-  // BoardDetail 페이지에 CommnetCount 속성이 있어서 사용하지않음. 나중에 뷰의 효율을 위해 변경 가능
-  //responseData.commentsCount = comments.count;
+   responseData.pictures = pictures;
+   responseData.lastPage =
+    Math.ceil(comments.count / 20) == 0 ? 1 : Math.ceil(comments.count / 20);
+    console.log(responseData.lastPage);
+   responseData.comments = comments.rows;
+   responseData.commentsCount = comments.count;
 
   return res.json(responseData);
 };
 
 const readComment = async (req, res, next) => {
-  var BO_Code = req.params.BO_Code;
+  var AL_Code = req.params.AL_Code;
   var offset = 0;
   var page = req.params.page;
 
@@ -77,18 +59,19 @@ const readComment = async (req, res, next) => {
     }
 
   var responseData = {};
-  comments = await FrontComment.findAndCountAll({
-    where: { Board_BO_Code: BO_Code },
+  comments = await A_Comment.findAndCountAll({
+    where: { Album_AL_Code: AL_Code },
     order: [
-      ["BC_Re_Ref", "DESC"],
-      ["BC_Code", "ASC"],
+      ["ACO_Code", "DESC"],
     ],
-    limit : 20, offset : offset
-  },);
+    limit : 20, offset: offset
+  });
   responseData.comments = comments.rows;
   responseData.commentsCount = comments.count;
   responseData.lastPage = Math.ceil(comments.count / 20);
   res.json(responseData);
 };
+
+
 
 module.exports = { readPost, readComment };
