5535ca3aeca552259f56e8a1efc6e58e5d6e010d
jpoh281 <54665433+jpoh281@users.noreply.github.com>
2020-08-04 12:28:03 +0900

    마이페이지 -> 게시판 활동내역 -> 댓글

--- 프로젝트/BackEnd/routes/board/posts/index.js
+++ 프로젝트/BackEnd/routes/board/posts/index.js
@@ -1,23 +1,28 @@
 var express = require("express");
 var router = express.Router();
+const downloadFile = require("../../../lib/sequelize/board/downloadFile");
 const writePost = require("../../../lib/sequelize/board/writeBoard").writePost;
 const upload = require("../../../lib/sequelize/board/writeBoard").upload;
 const readPost = require("../../../lib/sequelize/board/readBoard").readPost;
-const deletePost = require("../../../lib/sequelize/board/deleteBoard")
-  .deletePost;
+const deletePost = require("../../../lib/sequelize/board/deleteBoard").deletePost;
 const checkLogin = require("../../../lib/check/checkData").checkLogin;
 const checkOwnBoard = require("../../../lib/check/checkData").checkOwnBoard;
 const checkPost = require("../../../lib/sequelize/board/checkBoard").checkPost;
 const reportPost = require("../../../lib/sequelize/board/reportPost");
 const pushHealthsee = require("../../../lib/sequelize/board/pushHealthsee");
-const updatePost = require("../../../lib/sequelize/board/updateBoard")
-  .updatePost;
+const updatePost = require("../../../lib/sequelize/board/updateBoard").updatePost;
+const usersComments = require("../../../lib/sequelize/board/readBoard").usersComments;
 
+
+router.get("/users/",usersComments)
 // 게시글 작성
 router.post("/", checkLogin, upload.array("files"), writePost, readPost);
 
+router.get("/download/:code", downloadFile);
+
 // 해당 BO_CODE의 게시글 조회
 router.get("/:BO_Code", readPost);
+// 해당 BO_CODE의 게시글 조회
 
 // 해당 BO_CODE의 게시글 삭제
 router.delete("/:BO_Code", checkPost, checkLogin, checkOwnBoard, deletePost);
@@ -29,7 +34,6 @@ router.patch(
   updatePost,
   readPost
 );
-
 // 신고
 router.post("/report", checkPost, checkLogin, reportPost);
 
