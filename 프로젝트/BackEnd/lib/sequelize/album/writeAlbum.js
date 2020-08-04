330ebc0b598996761947534cd35111a969d3ab36
JunGeunHong1129 <54523136+JunGeunHong1129@users.noreply.github.com>
2020-08-04 16:34:41 +0900

    08/04 홍준근
    커맨트 추가

--- 프로젝트/BackEnd/lib/sequelize/album/writeAlbum.js
+++ 프로젝트/BackEnd/lib/sequelize/album/writeAlbum.js
@@ -1,5 +1,5 @@
 const Album = require("../../../models").album;
-const picture = require("../../../models").picture;
+const picture = require("../../../models").a_picture;
 const A_Comment = require("../../../models").a_comment;
 
 const multer = require("multer");
@@ -10,13 +10,15 @@ const upload = multer({
   limits: { fileSize: 1000000000 },
 });
 
+
 const writeAlbum = async (req, res, next) =>{
+
   let {
     Account_AC_NickName,
     AL_Content,
     AL_Scope
   } = req.body;
-
+  
   let album = await Album.create({
     Account_AC_NickName,
     AL_Content,
@@ -28,7 +30,8 @@ const writeAlbum = async (req, res, next) =>{
     for(var i = 0 ; i < req.files.length; i++){
 
       await picture.create({
-          P_Picture : req.files[i].buffer,
+          AP_Picture : req.files[i].buffer,
+          AP_Picture_Type: req.files[i].mimetype,
           Album_AL_Code: album.AL_Code
       });
 
@@ -40,4 +43,30 @@ const writeAlbum = async (req, res, next) =>{
   next();
   // res.end();
 }
-module.exports = { writeAlbum, upload };
+
+const writeComment = async (req, res, next) => {
+  let ACO_Content = req.body.ACO_Content;
+  let Album_AL_Code = req.body.Album_AL_Code;
+  let ACO_Writer_NickName = req.body.user.username;
+  let ACO_Creation_Date = today;
+  let Album_Account_AC_NickName = req.body.Album_Account_AC_NickName;
+
+
+
+  await A_Comment.create({
+    ACO_Content,
+    Album_AL_Code,
+    ACO_Writer_NickName,
+    ACO_Creation_Date,
+    Album_Account_AC_NickName
+  });
+
+  
+  req.params.AL_Code = Album_AL_Code;
+  req.params.page = req.body.page ? req.body.page : 1;
+
+  next();
+};
+
+module.exports = { writeAlbum, writeComment, upload };
+
