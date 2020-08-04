d6d3383d8a86b453b4047937022103565a912d33
JunGeunHong1129 <54523136+JunGeunHong1129@users.noreply.github.com>
2020-08-04 16:37:27 +0900

    08/04 홍준근
    checkOwnAlbum 추가

--- 프로젝트/BackEnd/lib/check/checkData.js
+++ 프로젝트/BackEnd/lib/check/checkData.js
@@ -1,8 +1,14 @@
+const Album = require("../../models").album;
+
 const checkLogin = (req, res, next) => {
   if (!req.body.user) {
     res.status(401).end();
     return;
   }
+  next();
+};
+
+const checkToken = (req, res, next) => {
   next();
 };
 
@@ -17,4 +23,23 @@ const checkOwnBoard = (req, res, next) => {
   next();
 };
 
-module.exports = { checkLogin, checkOwnBoard };
+const checkOwnAlbum = async (req, res, next)  => {
+  const { user, writerNickName ,AL_Code } = req.body;
+  console.log(req.body);
+  if(req.method === "POST"){
+    let album = await Album.findOne({
+      where : {AL_Code}
+    });
+    if(album.Account_AC_NickName !== user.username){
+      res.status(403).end();
+      return;
+    }
+  }else if (writerNickName !== user.username) {
+    res.status(403).end();
+    return;
+  }
+  next();
+};
+
+module.exports = { checkLogin, checkOwnBoard, checkOwnAlbum, checkToken };
+
