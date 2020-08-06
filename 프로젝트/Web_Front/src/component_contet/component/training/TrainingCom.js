3abbd0cd417b04ee5101b384c5760e55021d78a9
SeoJaeWan <sjw7324@gmail.com>
2020-08-06 16:38:12 +0900

    학습 AI 정확도를 좀 더 올렸다.

--- 프로젝트/Web_Front/src/component_contet/component/training/TrainingCom.js
+++ 프로젝트/Web_Front/src/component_contet/component/training/TrainingCom.js
@@ -4,22 +4,29 @@ import styled from "styled-components";
 
 const TrainingCom = ({ setup, draw, info }) => {
   return (
-    <TrainContainer>
-      <div className="trainImg">
-        <p>{info.my && info.my}</p>
+    <>
+      <TrainBox>
+        <div className="trainImg">
+          <p>{info.poses[info.nextPose]}</p>
+        </div>
+        <div className="trainCapture">
+          {/* {console.log(pose)} */}
+          <Sketch
+            setup={setup}
+            draw={(p5) => draw(p5, info.pose, info.skeleton)}
+          />
+        </div>
+      </TrainBox>
+
+      <div>
+        <div>asdsdaads</div>
+        <div>sadsadasd</div>
       </div>
-      <div className="trainCapture">
-        {/* {console.log(pose)} */}
-        <Sketch
-          setup={setup}
-          draw={(p5) => draw(p5, info.pose, info.skeleton)}
-        />
-      </div>
-    </TrainContainer>
+    </>
   );
 };
 
-const TrainContainer = styled.div`
+const TrainBox = styled.div`
   width: 100%;
 
   padding-top: 100px;
--- 프로젝트/Web_Front/src/containers/training/TrainingForm.js
+++ 프로젝트/Web_Front/src/containers/training/TrainingForm.js
@@ -5,17 +5,17 @@ import * as ml5 from "ml5";
 import AlertModal from "../../component_contet/common/Modal/AlertModal";
 
 const TrainingForm = ({ history }) => {
-  let brain;
-  let poseNet;
+  let brain; // AI를 사용하기 위한 변수
+  let poseNet; // 카메라를 통해 사용자의 동작을 입력받음
 
-  let [view, setView] = useState(true);
+  let [view, setView] = useState(true); // 시작 전 모달창을 출력시카기 위해서 사용
   let [info, setInfo] = useState({
+    // 사용자의 운동 정보 및 운동 자체에 대한 정보를 가지고 있음
     capture: null,
     pose: null,
-    skeleton: null,
     count: 0,
     nextPose: 0,
-    poses: null,
+    poses: [],
   });
 
   let poses;
@@ -23,10 +23,14 @@ const TrainingForm = ({ history }) => {
   let pose;
   let skeleton;
 
+  let bad = true;
+
+  // 학습 결과물을 로드하여 판단하는 재귀 함수를 실행시킨다.
   const brainLoaded = () => {
     console.log("pose classification ready!");
 
     poses = brain.neuralNetworkData.meta.outputs[0].uniqueValues;
+    poses.pop();
     poses = poses.concat(poses[0]);
 
     setInfo((pre) => ({ ...pre, poses }));
@@ -36,7 +40,12 @@ const TrainingForm = ({ history }) => {
     classifyPose(0, beforePose, 0);
   };
 
+  // 무한 재귀함수를 통해 동작은 계속 판단
+  // state : 해야할 포즈
+  // beforePose : 이전 포즈
+  // samePose : 같은 위치에 있는 경우 탈출을 하기위한 변수
   const classifyPose = (state, beforePose, samePose) => {
+    // pose가 확인될 경우
     if (pose) {
       let inputs = [];
       for (let i = 0; i < pose.keypoints.length; i++) {
@@ -48,51 +57,48 @@ const TrainingForm = ({ history }) => {
 
       beforePose = pose;
       brain.classify(inputs, (error, results) =>
-        gotResult(error, results, state, beforePose, samePose)
+        gotResult(results, state, beforePose, samePose)
       );
+      // pose가 없다면 다시 자기 자신을 호출
     } else {
       setTimeout(() => classifyPose(state, beforePose, samePose), 100);
     }
   };
 
-  const gotResult = (error, results, state, beforePose, samePose) => {
-    console.log(
-      results,
-      "내포즈",
-      results[0].label,
-      "해야하는 포즈",
-      poses[state],
-      results[0].confidence > 0.75,
-      results[0].confidence,
-      "sdasadsadadssadsaddsaasdsadsadsadsadsad",
-      results[0].label === poses[state],
-      results[0],
-
-      state
-    );
-
+  // 동작 검증
+  const gotResult = (results, state, beforePose, samePose) => {
+    // 동작의 정확도가 75% 이상이며 해야할 동작과 같은 경우
     if (results[0].confidence > 0.75 && results[0].label === poses[state]) {
       state = state + 1;
+      // state이 poses.length와 같다면 한 동작을 완료하였다고 판단
       if (state === poses.length) {
-        state = 1;
+        state = 0; // 상태를 0으로 만든 후 count를 증가
         setInfo((pre) => ({ ...pre, count: pre.count + 1, nextPose: state }));
       } else {
         setInfo((pre) => ({ ...pre, nextPose: state }));
       }
+      // 동작이 배드일 경우 state을 0으로 초기화
+    } else if (results[0].label === "배드" && bad) {
+      state = 0;
+      bad = !bad; // 동시에 여러번 bad가 뜰경우 진행이 불가능 하니 제한을 걸어줌
+
+      // 5초동안 bad가 나오지 않게 설정
+      setTimeout(() => {
+        bad = !bad;
+      }, 5000);
     }
 
+    // 한 동작에서 멈춰있을 경우(화면 밖, 종료 같은 상황) 탈출
     if (pose.score === beforePose.score) {
       samePose++;
       if (samePose === 100) {
         return;
       }
     } else samePose = 0;
-
-    setInfo((pre) => ({ ...pre, my: results[0].label }));
-    // console.log(results[0].confidence > 0.75, results[0].label.toUpperCase());
     classifyPose(state, beforePose, samePose);
   };
 
+  // 카메라를 통해 사용자의 동작을 입력받는 함수
   const gotPoses = (poses) => {
     if (poses.length > 0) {
       pose = poses[0].pose;
@@ -100,7 +106,6 @@ const TrainingForm = ({ history }) => {
       setInfo((pre) => ({
         ...pre,
         pose,
-        skeleton,
       }));
       // if (state === "collecting") {   사용 할수록 성능이 증가하게끔 만드는건 고려중
       //   console.log(pose);
@@ -147,9 +152,9 @@ const TrainingForm = ({ history }) => {
       setView(false);
       brain = ml5.neuralNetwork(options);
       const modelInfo = {
-        model: process.env.PUBLIC_URL + "/3pose_Test3/model.json",
-        metadata: process.env.PUBLIC_URL + "/3pose_Test3/model_meta.json",
-        weights: process.env.PUBLIC_URL + "/3pose_Test3/model.weights.bin",
+        model: process.env.PUBLIC_URL + "/3pose_Test2/model.json",
+        metadata: process.env.PUBLIC_URL + "/3pose_Test2/model_meta.json",
+        weights: process.env.PUBLIC_URL + "/3pose_Test2/model.weights.bin",
       };
       brain.load(modelInfo, brainLoaded);
     }, 1000);
@@ -189,10 +194,6 @@ const TrainingForm = ({ history }) => {
 
   return (
     <>
-      <h1>
-        {info.count}
-        {info.poses}
-      </h1>
       <TrainingCom setup={setup} draw={draw} info={info} />
 
       <AlertModal
