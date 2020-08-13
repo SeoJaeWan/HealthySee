c3b8b92835f8e4d8b92e0bd0df9d403b5d9efa1b
JunGeunHong1129 <54523136+JunGeunHong1129@users.noreply.github.com>
2020-08-13 15:17:34 +0900

    08/13 홍준근 checkPost 검사 서순 고침

--- 프로젝트/BackEnd/routes/album/posts/index.js
+++ 프로젝트/BackEnd/routes/album/posts/index.js
@@ -3,19 +3,18 @@ var router = express.Router();
 const upload = require("../../../lib/sequelize/album/writeAlbum").upload;
 const writePost = require("../../../lib/sequelize/album/writeAlbum").writeAlbum;
 const readPost = require("../../../lib/sequelize/album/readAlbum").readPost;
-const checkPost = require("../../../lib/sequelize/album/checkAlbum").checkPost;
+const checkPost = require("../../../lib/check/album/checkAlbum").checkPost;
 const checkLogin = require("../../../lib/check/checkData").checkLogin;
-const checkOwnBoard = require("../../../lib/check/checkData").checkOwnBoard;
 const deletePost = require("../../../lib/sequelize/album/deleteAlbum").deletePost;
 const updatePost = require("../../../lib/sequelize/album/updateAlbum").updateAlbum;
 
 router.post("/" , checkLogin, upload.array("files"), writePost, readPost);
 
-router.get("/:AL_Code",checkLogin, readPost);
+router.get("/:AL_Code" , checkLogin, checkPost, readPost);
 
 
 
-router.delete("/:AL_Code", checkPost, checkLogin, checkOwnBoard, deletePost);
+router.delete("/:AL_Code", checkLogin, checkPost, deletePost);
 
 router.patch("/:AL_Code", checkPost, upload.array("files"), updatePost, readPost);
 
