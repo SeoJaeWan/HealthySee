87c1d8c84ac93aff3f494898dd4b36b19332faae
JunGeunHong1129 <54523136+JunGeunHong1129@users.noreply.github.com>
2020-08-13 15:16:27 +0900

    08/13 홍준근 checkPost 검사 추가

--- 프로젝트/BackEnd/routes/album/comments/index.js
+++ 프로젝트/BackEnd/routes/album/comments/index.js
@@ -3,43 +3,43 @@ var router = express.Router();
 const writeComment = require("../../../lib/sequelize/album/writeAlbum").writeComment;
 const readComment = require("../../../lib/sequelize/album/readAlbum").readComment;
 const reportComment = require("../../../lib/sequelize/album/reportAlbumComment");
-const checkComment = require("../../../lib/sequelize/album/checkAlbum").checkComment;
-const checkOwnAlbum = require("../../../lib/check/checkData").checkOwnAlbum;
+const checkComment = require("../../../lib/check/album/checkAlbum").checkComment;
 const deleteComment = require("../../../lib/sequelize/album/deleteAlbum").deleteComment;
 const updateComment = require("../../../lib/sequelize/album/updateAlbum").updateComment;
 const checkLogin = require("../../../lib/check/checkData").checkLogin;
+const checkPost = require("../../../lib/check/album/checkAlbum").checkPost;
 
 //댓글 쓰기
-router.post("/", writeComment,readComment);
+router.post("/:AL_Code",checkPost, writeComment,readComment);
 
 //댓글 가져오기
-router.get("/:AL_Code&:page", readComment);
+router.get("/:AL_Code&:page", checkPost, readComment);
 
 //댓글 수정
 router.patch(
-  "/",
+  "/:AL_Code",
+  checkPost,
   checkComment,
   checkLogin,
-  checkOwnAlbum,
   updateComment,
   readComment
 );
 
 //댓글 차단
 router.post(
-  "/report",
+  "/report/:AL_Code",
+  checkPost,
   checkComment,
-  checkOwnAlbum,
   reportComment,
   readComment
 );
 
 //댓글 삭제
 router.delete(
-    "/:ACO_Code&:page",
+    "/:AL_Code&:ACO_Code&:page",
     checkLogin,
+    checkPost,
     checkComment,
-    checkOwnAlbum,
     deleteComment,
     readComment
   );
