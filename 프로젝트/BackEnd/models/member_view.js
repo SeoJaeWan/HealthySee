aacbd0a9d3097452ff23d35f61d6b354cb0b441b
jpoh281 <54665433+jpoh281@users.noreply.github.com>
2020-08-21 16:11:21 +0900

    Update member_view.js

--- 프로젝트/BackEnd/models/member_view.js
+++ 프로젝트/BackEnd/models/member_view.js
@@ -8,18 +8,15 @@ module.exports = function (sequelize, DataTypes) {
           primaryKey: true,
           autoIncrement: true,
         },
-        ME_Profile_Photo: {
-          type: DataTypes.BLOB("long"),
-          allowNull: true,
-        },
-        ME_Profile_Type: {
+        Account_AC_NickName: {
           type: DataTypes.STRING(16),
-          allowNull: true,
-        },
-        ME_Scope: {
-          type: DataTypes.INTEGER,
           allowNull: false,
-          defaultValue: "0",
+          primaryKey: true,
+          // references: {
+          //   model: "account",
+          //   key: "AC_NickName",
+  
+          // },
         },
         ME_Weight: {
           type: DataTypes.FLOAT,
@@ -31,32 +28,39 @@ module.exports = function (sequelize, DataTypes) {
           allowNull: false,
           defaultValue: "0",
         },
-        ME_Birth: {
-          type: DataTypes.DATE,
-          allowNull: true,
-        },
         ME_Gender: {
           type: DataTypes.INTEGER,
           allowNull: false,
           defaultValue: "1",
         },
-        Account_AC_NickName: {
-          type: DataTypes.STRING(16),
+        ME_Scope: {
+          type: DataTypes.INTEGER,
           allowNull: false,
-          primaryKey: true,
-          references: {
-            model: "account",
-            key: "AC_NickName",
-  
-          },
+          defaultValue: "0",
         },
         ME_Board_Count : {
             type : DataTypes.INTEGER,
             allowNull : false,
         },
-        ME_Album_Count : {
-            type : DataTypes.INTEGER,
-            allowNull : false,
+        ME_My_Album_Count : {
+          type : DataTypes.INTEGER,
+          allowNull : false,
+      },
+      ME_Others_Album_Count : {
+        type : DataTypes.INTEGER,
+        allowNull : false,
+    },
+        ME_Birth: {
+          type: DataTypes.DATE,
+          allowNull: true,
+        },
+        ME_Profile_Photo: {
+          type: DataTypes.BLOB("long"),
+          allowNull: true,
+        },
+        ME_Profile_Type: {
+          type: DataTypes.STRING(16),
+          allowNull: true,
         },
       },
       {
@@ -65,4 +69,3 @@ module.exports = function (sequelize, DataTypes) {
       }
     );
   };
-  
