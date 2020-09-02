32c1b47761917bf4489fa7f49c0a7872a42ea143
hanapple07 <54665408+hanapple07@users.noreply.github.com>
2020-09-02 14:32:44 +0900

    엘범 추가

--- 프로젝트/Web_Front/src/Page/Album.js
+++ 프로젝트/Web_Front/src/Page/Album.js
@@ -1,17 +1,18 @@
 import React from "react"
 import { Route } from "react-router-dom"
 import AlbumList from "../component_contet/component/album/AlbumList"
-import AlbumWrite from "../component_contet/component/album/AlbumWrite"
+import AlbumWriteForm from "../containers/album/AlbumWriteForm"
 import AlbumEditCom from "../component_contet/component/album/AlbumEditCom"
 
 const Album = ({ match }) => {
   return (
     <>
       <Route exact path={match.path} component={AlbumList} />
-      <Route path={`${match.path}/Write`} component={AlbumWrite} />
+      <Route path={`${match.path}/Write`} component={AlbumWriteForm} />
       <Route path={`${match.path}/Edit`} component={AlbumEditCom} />
     </>
   )
 }
 
 export default Album
+
