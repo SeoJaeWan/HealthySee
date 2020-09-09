00c2db80f4f9a2efafe7025c7c3b08ffa3da9778
JunGeunHong <54523136+JunGeunHong1129@users.noreply.github.com>
2020-09-09 16:05:18 +0900

    09/09 홍준근 플랜 평가 라우트 수정

--- 프로젝트/BackEnd/routes/plan/evaluations/index.js
+++ 프로젝트/BackEnd/routes/plan/evaluations/index.js
@@ -4,37 +4,37 @@ const writeEvaluation = require("../../../lib/sequelize/plan/writePlan")
   .writeEvaluation;
 const readEvaluation = require("../../../lib/sequelize/plan/readPlan")
   .readEvaluation;
-const checkLogin = require("../../../lib/check/checkData").checkLogin;
-const checkOwnBoard = require("../../../lib/check/checkData").checkOwnBoard;
+const checkLogin = require("../../../lib/check/user").checkLogin;
 const checkEvaluation = require("../../../lib/check/plan").checkEvaluation;
 const deleteEvaluation = require("../../../lib/sequelize/plan/deletePlan")
   .deleteEvaluation;
 const updateEvaluation = require("../../../lib/sequelize/plan/updatePlan")
   .updateEvaluation;
 const reportEvaluation = require("../../../lib/sequelize/plan/reportPlanEvaluation");
+const  checkPlan  = require("../../../lib/check/plan").checkPlan;
 // 댓글 작성
-router.post("/", writeEvaluation, readEvaluation);
+router.post("/:PL_Code", checkPlan, writeEvaluation, readEvaluation);
 
-router.post("/report", reportEvaluation, readEvaluation);
+router.post("/report/:PL_Code",checkPlan, checkEvaluation, reportEvaluation, readEvaluation);
 
 // 해당 BO_CODE의 댓글 조회
 router.get("/:PL_Code&:page", readEvaluation);
 
 router.patch(
-  "/",
+  "/:PL_Code",
+  checkPlan,
   checkEvaluation,
   checkLogin,
-  checkOwnBoard,
   updateEvaluation,
   readEvaluation
 );
 
 //router.delete("/:BC_Code",deleteComment, readComment);
 router.delete(
-  "/:PEV_Code&:page",
+  "/:PL_Code&:PEV_Code&:page",
+  checkPlan,
   checkEvaluation,
   checkLogin,
-  checkOwnBoard,
   deleteEvaluation,
   readEvaluation
 );
