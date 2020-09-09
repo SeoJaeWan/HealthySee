9a62b2c6b9ac6eec80e52f0ffcb79f593535c2fa
JunGeunHong <54523136+JunGeunHong1129@users.noreply.github.com>
2020-09-09 12:08:50 +0900

    09/09 플랜관련 check 모듈 추가

--- 프로젝트/BackEnd/lib/check/plan/index.js
+++ 프로젝트/BackEnd/lib/check/plan/index.js
@@ -1,25 +1,25 @@
-const Plan = require("../../../models").plan;
+const PlanDetail = require("../../../models").plandetail;
+const P_Evaluation = require("../../../models").p_evaluation;
 
-// Plan Code가 존재하는지 확인
-const checkOwnPlan = async (req, res, next) => {
-  const { planCode } = req.body.training;
+const checkPlan = async (req, res, next) => {
+  const PL_Code = req.params.PL_Code ? req.params.PL_Code : req.body.PL_Code;
 
-  console.log("asdsadsad", planCode);
-
-  if (!planCode) {
-    next();
+  if (!PL_Code) {
+    res.status(400).end();
+    return;
   }
+  console.log(PL_Code);
 
   try {
-    let plan = await Plan.findOne({
-      where: { PL_Code: planCode },
-    });
+    const plan = await PlanDetail.findOne({ where: { PL_Code: PL_Code } });
 
     if (!plan) {
       res.status(404).end();
       return;
     }
+    console.log(plan);
 
+    req.body.writerNickName = plan.PL_Writer_NickName;
     next();
   } catch (e) {
     console.log(e);
@@ -27,6 +27,33 @@ const checkOwnPlan = async (req, res, next) => {
   }
 };
 
-module.exports = {
-  checkOwnPlan,
+const checkEvaluation = async (req, res, next) => {
+  let PEV_Code;
+
+  if (req.method === "PATCH" || req.method === "POST")
+  PEV_Code = req.body.PEV_Code;
+  else PEV_Code = req.params.PEV_Code;
+
+  console.log(PEV_Code);
+
+  if (!PEV_Code) {
+    res.status(400).end();
+    return;
+  }
+
+  try {
+    const evaluation = await P_Evaluation.findOne({ where: { PEV_Code } });
+
+    if (!evaluation) {
+      res.status(404).end();
+      return;
+    }
+
+    req.body.writerNickName = evaluation.PEV_Writer_NickName;
+    next();
+  } catch (error) {
+    res.status(500).end();
+  }
 };
+
+module.exports = { checkPlan, checkEvaluation };
