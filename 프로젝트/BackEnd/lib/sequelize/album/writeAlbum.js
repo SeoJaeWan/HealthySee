573993e7d111a5f922aa6b03bba3ee6098817164
JunGeunHong <54523136+JunGeunHong1129@users.noreply.github.com>
2020-09-11 16:09:24 +0900

    09/11 홍준근 엘범포스트 작성시 마지막 썸네일용 사진 추가 기능 추가

--- 프로젝트/BackEnd/lib/sequelize/album/writeAlbum.js
+++ 프로젝트/BackEnd/lib/sequelize/album/writeAlbum.js
@@ -10,25 +10,33 @@ const upload = multer({
   limits: { fileSize: 1000000000 },
 });
 
-const writeAlbum = async (req, res, next) => {
-  let { Account_AC_NickName, AL_Content, AL_Scope } = req.body;
 
-  console.log(req.body);
+const writeAlbum = async (req, res, next) =>{
+
+  let {
+    Account_AC_NickName,
+    AL_Content,
+    AL_Scope
+  } = req.body;
+  
 
   let album = await Album.create({
     Account_AC_NickName,
+    AL_Thumbnail : req.files[req.files.length-1].buffer,
     AL_Content,
     AL_Scope,
-    AL_Creation_Date: today,
+    AL_Creation_Date : today
   });
+  //'2020-09-18 12:00:00'
 
-  if (req.files) {
-    for (var i = 0; i < req.files.length; i++) {
+  if(req.files){
+    for(var i = 0 ; i < req.files.length-1; i++){
       await picture.create({
-        AP_Picture: req.files[i].buffer,
-        AP_Picture_Type: req.files[i].mimetype,
-        Album_AL_Code: album.AL_Code,
+          AP_Picture : req.files[i].buffer,
+          AP_Picture_Type: req.files[i].mimetype,
+          Album_AL_Code: album.AL_Code
       });
+
     }
   }
 
@@ -36,7 +44,7 @@ const writeAlbum = async (req, res, next) => {
   req.body.self = true;
   next();
   // res.end();
-};
+}
 
 const writeComment = async (req, res, next) => {
   let ACO_Content = req.body.ACO_Content;
@@ -44,15 +52,19 @@ const writeComment = async (req, res, next) => {
   let ACO_Writer_NickName = req.body.user.username;
   let ACO_Creation_Date = today;
   let Album_Account_AC_NickName = req.body.Album_Account_AC_NickName;
+  
+
+
 
   await A_Comment.create({
     ACO_Content,
     Album_AL_Code,
     ACO_Writer_NickName,
     ACO_Creation_Date,
-    Album_Account_AC_NickName,
+    Album_Account_AC_NickName
   });
 
+  
   req.params.AL_Code = Album_AL_Code;
   req.params.page = req.body.page ? req.body.page : 1;
 
@@ -60,3 +72,4 @@ const writeComment = async (req, res, next) => {
 };
 
 module.exports = { writeAlbum, writeComment, upload };
+
