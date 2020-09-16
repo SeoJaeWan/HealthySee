9230cd9b1ed5cb43c9d81431d81b50e86d7bbef4
hanapple07 <54665408+hanapple07@users.noreply.github.com>
2020-09-16 21:05:50 +0900

    Add files via upload

---
+++ 프로젝트/Web_Front/src/component_contet/component/set/style/SetPoseItemCom_style.js
@@ -0,0 +1,93 @@
+import styled, { keyframes } from "styled-components"
+import { media } from "../../../../lib/ReactiveStyle/ReactiveStyle"
+
+export const SetPoseItemForm = styled.div`
+  width: 20%;
+  min-width: 20rem;
+  margin: 1.2rem;
+  height: 25rem;
+  border: 3px solid #676a72;
+  border-radius: 20px;
+  ${media.mobile`
+    min-width: 15rem;
+    height: 20rem;
+  `}
+  :hover {
+    cursor: pointer;
+  }
+
+  .profileIMG {
+    width: 100px;
+    height: 100px;
+    border-radius: 20px;
+    margin: 10px;
+    ${media.mobile`
+    width: 50px;
+    height: 50px;
+  `}
+  }
+
+  .titleForms {
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    ${media.mobile`
+    height: 5rem;
+  `}
+  }
+
+  .name {
+    width: 100%;
+    margin: 0;
+    text-align: center;
+    font-size: 2.5rem;
+  }
+
+  h3 {
+    flex-grow: 1;
+    text-align: center;
+    margin: 1rem;
+  }
+
+  .moreButton {
+    font-size: 3rem;
+    height: 3rem;
+    line-height: 3rem;
+    width: 20%;
+  }
+
+  .imgView {
+    margin: 0 auto;
+    max-width: 100%;
+    max-height: 100%;
+    ${media.mobile`
+    margin: 0 auto;
+    width:12rem;
+    height:13rem;
+  `}
+  }
+  .contents {
+    margin: 1rem;
+    max-height: 18rem;
+    width: 17.5rem;
+    border-radius: 20px;
+    overflow: hidden;
+    border: 3px solid #676a72;
+    ${media.mobile`
+    margin-top:0.5rem;
+    width:12rem;
+    height:13rem;
+  `}
+  }
+
+  img {
+    max-width: 100%;
+  }
+
+  .menu-item-wrapper {
+    width: 17.5rem;
+    vertical-align: middle;
+  }
+  .menu-wrapper {
+  }
+`
---
+++ 프로젝트/Web_Front/src/component_contet/component/set/style/SetWriteCom_style.js
@@ -0,0 +1,123 @@
+import styled, { keyframes } from "styled-components"
+import { media } from "../../../../lib/ReactiveStyle/ReactiveStyle"
+
+export const SetWriteForm = styled.div`
+  width: 98%;
+  height: 35rem;
+  margin: 1rem;
+  border-radius: 20px;
+  ${media.mobile`
+    min-width: 15rem;
+    height: 20rem;
+  `}
+
+  .titleFrom {
+    width: 100%;
+    position: relative;
+    height: 5rem;
+    display: flex;
+    align-items: center;
+    margin-top: 1rem;
+    ${media.mobile`
+    height: 5rem;
+  `}
+  }
+  h1 {
+    margin: 0 2rem;
+    font-size: 2.5rem;
+  }
+
+  .titleInput {
+    width: 60%;
+    height: 3.5rem;
+    font-size: 2rem;
+  }
+
+  .writeButton {
+    position: absolute;
+    font-size: 1.8rem;
+    top: 0px;
+    right: 10px;
+  }
+
+  .addButton {
+    position: absolute;
+    font-size: 1.8rem;
+    top: 40px;
+    right: 10px;
+  }
+
+  .content {
+    margin: 1rem auto;
+    height: 28rem;
+    width: 95%;
+    border-radius: 20px;
+    display: flex;
+    overflow: hidden;
+    overflow-x: hidden;
+    border: 3px solid #676a72;
+    ${media.mobile`
+    margin-top:0.5rem;
+    width:12rem;
+    height:13rem;
+  `}
+  }
+
+  .explainSetForm {
+    font-size: 2rem;
+  }
+  .setTitle {
+    margin: 2rem 0 0 3rem;
+  }
+  .textForm {
+    display: flex;
+
+    width: 100%;
+    height: 400px;
+
+    font-size: 1rem;
+
+    border: 5px solid #676a72;
+    border-left: solid white;
+
+    margin: 20px 0 30px 0;
+
+    ${media.mobile`
+    width: 100%;
+    margin-left: 2%;
+    `}
+    &::before {
+      content: "";
+      position: static;
+      margin: -10px;
+      margin-top: -30px;
+      display: block;
+      border-radius: 50%;
+      width: 50px;
+      height: 58px;
+      background: #676a72;
+      box-shadow: 1px 3px 3px 1px rgba(0, 0, 0, 0.2);
+      transition: 0.2s;
+      ${media.mobile`    
+      margin-top: -20px;
+      z-index:3;
+      width: 30px;
+      height: 40px;
+  `}
+    }
+  }
+  .textArea {
+    margin-left: 2%;
+    padding: 8px;
+
+    color: #676a72;
+    font: inherit;
+    font-size: 1.5rem;
+    width: 100%;
+    border: none;
+    resize: none;
+    overflow: auto;
+    height: auto;
+    box-shadow: 0px 4px 10px -8px black;
+  }
+`
